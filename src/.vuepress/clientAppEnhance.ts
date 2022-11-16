e8cefda98af8aea9653cb5f3f82246fc176f071e
Sherlock <shh_name@hotmail.com>
2022-11-16 16:25:46 +0800

    XY

--- src/.vuepress/clientAppEnhance.ts
+++ src/.vuepress/clientAppEnhance.ts
@@ -3,5 +3,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
-  app.use(ElementPlus)
+  app.use(ElementPlus),
+  router.beforeEach((to) => {
+    if (typeof _paq !== "undefined") {
+      if (to.path) {
+        _paq.push(["trackPageView", to.fullPath])
+      }
+    }
+  })  
 })
